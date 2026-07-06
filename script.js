79a640fc4286a04edc22b91c8ccbbf6b71aec998
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-06 09:20:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,70 @@ window.addEventListener("load", () => {
   },1800);
 
 });
+// ==========================
+// STAR BACKGROUND
+// ==========================
+
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+let stars=[];
+
+function resize(){
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+}
+
+resize();
+
+window.addEventListener("resize",resize);
+
+for(let i=0;i<120;i++){
+
+stars.push({
+
+x:Math.random()*canvas.width,
+
+y:Math.random()*canvas.height,
+
+r:Math.random()*2,
+
+s:Math.random()*0.6+0.2
+
+});
+
+}
+
+function draw(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ctx.fillStyle="white";
+
+stars.forEach(star=>{
+
+ctx.beginPath();
+
+ctx.arc(star.x,star.y,star.r,0,Math.PI*2);
+
+ctx.fill();
+
+star.y+=star.s;
+
+if(star.y>canvas.height){
+
+star.y=0;
+
+star.x=Math.random()*canvas.width;
+
+}
+
+});
+
+requestAnimationFrame(draw);
+
+}
+
+draw();
